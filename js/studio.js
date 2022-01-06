7b51784df10a22178c9694bec0f6fc0ac8ec776b
Peter Collingridge <peter.collingridge@nanoporetech.com>
2022-01-06 22:10:09 +0000

    Try frame-tracker component

--- js/studio.js
+++ js/studio.js
@@ -37,15 +37,15 @@ Vue.component('attribute-control', {
             <line
                 v-for="i in controlPoints.length - 1"
                 class="control-line"
-                :x1="controlPoints[i - 1].x * 10"
+                :x1="getX(controlPoints[i - 1].x)"
                 :y1="controlPoints[i - 1].y"
-                :x2="controlPoints[i].x * 10"
+                :x2="getX(controlPoints[i].x)"
                 :y2="controlPoints[i].y"
             />
             <control-point
                 v-for="(controlPoint, index) in controlPoints"
                 :key="index"
-                :x="controlPoint.x * 10"
+                :x="getX(controlPoint.x)"
                 :y="controlPoint.y"
                 :index="index"
             />
@@ -57,6 +57,9 @@ Vue.component('attribute-control', {
         }
     },
     methods: {
+        getX: function(x) {
+            return this.$parent.getX(x);
+        },
         drag: function(evt) {
             if (this.selected !== false) {
                 const controlPoint = this.controlPoints[this.selected];
@@ -69,10 +72,51 @@ Vue.component('attribute-control', {
     },
 });
 
+Vue.component('frame-tracker', {
+    // props: ['numFrames'],
+    data: function() {
+        return {
+            numFrames: 100,
+        }
+    },
+    template: `
+        <svg class="frame-tracker" viewBox="0 0 1010 20">
+            <rect class="background" width="1010" height="20" />
+            <text v-for="frame in frames" :x="getX(frame)" y="13">
+                {{frame}}
+            </text>
+            <line class="tick-mark" v-for="i in numFrames" :x1="i * 10" :x2="i * 10" y1="15" y2="20" />
+        </svg>
+    `,
+    computed: {
+        frames: function() {
+            const dFrames = 10;
+            const frames = [1];
+            for (let i = dFrames; i <= this.numFrames; i += dFrames) {
+                frames.push(i);
+            }
+            return frames;
+        },
+    },
+    methods: {
+        getX: function(frame) {
+            return this.$parent.getX(frame);
+        }
+    }
+});
+
 const app = new Vue({
     el: '#app',
     data: {
-        numFrames: 30,
+        numFrames: 100,
         height: 100,
     },
+    methods: {
+        getX: function(x) {
+            // Map frames 1 to numFrames to range 0 - 1000 with some padding
+            const padding = 10;
+            const dx = (1000 - padding * 2) / (this.numFrames - 1);
+            return padding + (x - 1) * dx;
+        }
+    }
 })
